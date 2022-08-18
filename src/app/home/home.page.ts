a140f40b7aeb5fa859a531febd33352efba08d07
unknown <brezo.al@gmail.com>
2022-08-18 15:37:29 +0200

    testing reader

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {
+  BarcodeScanner,
+  BarcodeScannerOptions,
+} from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  scannedData: any;
+  encodedData: '';
+  encodeData: any;
+  inputData: any;
 
-  constructor() {}
+  constructor(private barcodeScanner: BarcodeScanner) {}
 
+  scanBarcode() {
+    const options: BarcodeScannerOptions = {
+      preferFrontCamera: false,
+      showFlipCameraButton: true,
+      showTorchButton: true,
+      torchOn: false,
+      prompt: 'Place a barcode inside the scan area',
+      resultDisplayDuration: 500,
+      formats: 'EAN_13,EAN_8,QR_CODE,PDF_417 ',
+      orientation: 'portrait',
+    };
+
+    this.barcodeScanner
+      .scan(options)
+      .then((barcodeData) => {
+        console.log('Barcode data', barcodeData);
+        this.scannedData = barcodeData;
+      })
+      .catch((err) => {
+        console.log('Error', err);
+      });
+  }
 }
